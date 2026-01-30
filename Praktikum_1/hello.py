3324db606783cfaa115515b85d0262153c097d22
diikd12 <hadifauzan255@gmmail.com>
2026-01-30 15:21:31 +0700

    adding hello.py

--- Praktikum_1/hello.py
+++ Praktikum_1/hello.py
@@ -1,1 +1,11 @@
-print("hello world!")
+nama = 'Hadi fauzan'
+kelas = 'TI-C'
+NIM = 25071207083
+Universitas = 'UNRI'
+Fakultas = 'Teknik'
+
+print ('Halo, nama saya ' + nama, 'saya dari kelas', kelas)
+print ('dengan NIM ', str(NIM))
+print ('Saya kuliah di ' + Universitas)
+print ('Fakultas ' + Fakultas)
+print ('Senang berkenalan dengan kalian semua!')
